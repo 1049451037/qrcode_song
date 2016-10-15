62453ab29b98f2b48e5346efa0e7c92e9d9555e8
1049451037 <1049451037@qq.com>
2016-10-15 09:35:13 +0800

    添加美化模式

--- qrcode.py
+++ qrcode.py
@@ -2,32 +2,33 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def qrcode_text_link(text):
+def qrcode_text_link(text,mode=0):
     if text=='':
         return None
     param = {'text':text}
+    if mode!=0:
+        param['mhid']='thGTD1zpnZohMHcuI9BUPqo'
     url = 'https://cli.im/api/qrcode/code'
     r = requests.get(url,params=param)
     soup = BeautifulSoup(r.text,'html.parser')
     img = soup.img
-    link = img['src']
-    return 'https://'+link[2:]
+    return 'https:'+img['src']
 
-def qrcode_text_png(text , filepath='./' , filename='qrcode_song.png'):
+def qrcode_text_png(text ,mode=0, filepath='./' , filename='qrcode_song.png'):
     if text=='':
         return None
-    link = qrcode_text_link(text)
+    link = qrcode_text_link(text,mode=mode)
     r = requests.get(link)
     with open(filepath+filename,'wb') as f:
         f.write(r.content)
 
-def qrcode_link_link(link):
-    return qrcode_text_link('//'+link)
+def qrcode_link_link(link,mode=0):
+    return qrcode_text_link('//'+link,mode=mode)
 
-def qrcode_link_png(link , filepath='./' , filename='qrcode2_song.png'):
+def qrcode_link_png(link , mode=0 , filepath='./' , filename='qrcode2_song.png'):
     if link=='':
         return None
-    _link = qrcode_link_link(link)
+    _link = qrcode_link_link(link,mode=mode)
     r = requests.get(_link)
     with open(filepath+filename,'wb') as f:
         f.write(r.content)
--- test.py
+++ test.py
@@ -4,6 +4,6 @@ from qrcode import qrcode_link_link
 from qrcode import qrcode_link_png
 
 print(qrcode_text_link('abc'))
-qrcode_text_png('abc')
+qrcode_text_png('abc',1) #调整美化模式
 print(qrcode_link_link('www.baidu.com'))
 qrcode_link_png('www.baidu.com')
