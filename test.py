e3c6d7654cf1104c25912c8f529dccc8e713c333
1049451037 <1049451037@qq.com>
2016-11-25 17:39:40 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
-from qrcode import qrcode_text_link
-from qrcode import qrcode_text_png
-from qrcode import qrcode_link_link
-from qrcode import qrcode_link_png
+import myqrcode
 
-print(qrcode_text_link('abc'))
-qrcode_text_png('abc',1) #调整美化模式
-print(qrcode_link_link('www.baidu.com'))
-qrcode_link_png('www.baidu.com')
+print(myqrcode.text_link(text='你好'))  # 文本二维码图片的链接
+myqrcode.text_png(text='你好', mode=1 ,filepath='./',filename='test1.png')  # 二维码图片保存到本地，mode调整美化模式
+print(myqrcode.link_link(link='www.baidu.com')) # 网址的二维码图片的链接
+myqrcode.link_png(link='www.baidu.com',filepath='./',filename='test2.png') #实际上函数还会返回图片的二进制存储
